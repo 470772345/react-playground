b6e8b7e48c7baf066e60135bdf3c5534b47e6835
林瑞涛 <linruitao@linruitaodeMacBook-Pro.local>
2024-08-04 15:35:03 +0800

    feat:theme

--- src/ReactPlayground/components/CodeEditor/index.tsx
+++ src/ReactPlayground/components/CodeEditor/index.tsx
@@ -5,7 +5,7 @@ import { PlaygroundContext } from "../../PlaygroundContext";
 import { debounce } from "lodash-es";
 
 export default function CodeEditor() {
-  const { files, setFiles, selectedFileName, setSelectedFileName } =
+  const { theme, files, setFiles, selectedFileName, setSelectedFileName } =
     useContext(PlaygroundContext);
 
   const file = files[selectedFileName];
@@ -18,7 +18,9 @@ export default function CodeEditor() {
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
       <FileNameList />
-      <Editor file={file} onChange={debounce(onEditorChange, 400)} />
+      <Editor file={file} onChange={debounce(onEditorChange, 400)} options={{
+                theme: `vs-${theme}`
+            }} />
     </div>
   );
 }
--- src/ReactPlayground/components/Preview/index.tsx
+++ src/ReactPlayground/components/Preview/index.tsx
@@ -3,7 +3,16 @@ import { PlaygroundContext } from "../../PlaygroundContext"
 import { compile } from "./compiler";
 import iframeRaw from './iframe.html?raw'
 import { IMPORT_MAP_FILE_NAME } from "../../files";
+import { Message } from "../Message";
 // import Editor from "../CodeEditor/Editor";
+
+
+interface MessageData {
+    data: {
+      type: string
+      message: string
+    }
+}
 
 export default function Preview() {
 
@@ -35,6 +44,24 @@ export default function Preview() {
 
     const [iframeUrl, setIframeUrl] = useState(getIframeUrl());
 
+
+    const [error, setError] = useState('')
+
+    const handleMessage = (msg: MessageData) => {
+        const { type, message } = msg.data
+        if (type === 'ERROR') {
+          setError(message)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('message', handleMessage)
+        return () => {
+          window.removeEventListener('message', handleMessage)
+        }
+    }, [])
+
+
     return <div style={{height: '100%'}}>
         <iframe
             src={iframeUrl}
@@ -45,6 +72,7 @@ export default function Preview() {
                 border: 'none',
             }}
         />
+        <Message type='error' content={error} />
         {/* <Editor file={{
             name: 'dist.js',
             value: compiledCode,
