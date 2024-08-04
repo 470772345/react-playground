23bfbb7afce9fe7b056a6ba78fc8d84dce14aac3
林瑞涛 <linruitao@linruitaodeMacBook-Pro.local>
2024-08-04 17:16:57 +0800

    feat:share link and download files

--- src/ReactPlayground/components/Header/index.tsx
+++ src/ReactPlayground/components/Header/index.tsx
@@ -1,36 +1,51 @@
-import styles from './index.module.scss'
+import styles from "./index.module.scss"
 
-import logoSvg from './icons/logo.svg';
-import { useContext } from 'react';
-import { PlaygroundContext } from '../../PlaygroundContext';
-import { MoonOutlined, SunOutlined } from '@ant-design/icons';
-
+import logoSvg from "./icons/logo.svg"
+import { useContext } from "react"
+import { PlaygroundContext } from "../../PlaygroundContext"
+import { MoonOutlined, SunOutlined, ShareAltOutlined , DownloadOutlined} from "@ant-design/icons"
+import { message } from "antd"
+import copy from "copy-to-clipboard"
+import { downloadFiles } from "../../utils"
 
 export default function Header() {
-  
-  const { theme, setTheme} = useContext(PlaygroundContext)
+  const { files, theme, setTheme } = useContext(PlaygroundContext)
 
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        <img alt='logo' src={logoSvg}/>
-        <span>React Playground - FE-茶叶蛋</span>
+        <img alt="logo" src={logoSvg} />
+        <span className={styles.title}>React Playground - FE-茶叶蛋</span>
       </div>
       <div className={styles.links}>
-        {theme === 'light' && (
+        {theme === "light" && (
           <MoonOutlined
-            title='切换暗色主题'
+            title="切换暗色主题"
             className={styles.theme}
-            onClick={() => setTheme('dark')}
+            onClick={() => setTheme("dark")}
           />
         )}
-        {theme === 'dark' && (
+        {theme === "dark" && (
           <SunOutlined
-            title='切换亮色主题'
+            title="切换亮色主题"
             className={styles.theme}
-            onClick={() => setTheme('light')}
+            onClick={() => setTheme("light")}
           />
         )}
+        <ShareAltOutlined
+          style={{ marginLeft: "10px" }}
+          onClick={() => {
+            copy(window.location.href)
+            message.success("分享链接已复制。")
+          }}
+        />
+        <DownloadOutlined 
+          style={{marginLeft: '10px'}}
+          onClick={async () => {
+            await downloadFiles(files);
+            message.success('下载完成')
+          }}
+        />
       </div>
     </div>
   )
